e4066c4258c057416fc38c23f622a26b2d3e504a
raid35 <136473494+raid35@users.noreply.github.com>
2026-02-06 15:23:34 +0100

    Update Models.kt

--- app/src/main/java/com/example/siksa/Models.kt
+++ app/src/main/java/com/example/siksa/Models.kt
@@ -1,6 +1,7 @@
 package com.example.siksa
 
 import android.os.Parcelable
+import android.util.Base64   // 👈 هنا تضيف الاستيراد
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,12 +10,27 @@ data class Channel(
     val url: String,
     val logo: String = "",
     val drmLicense: String = "",
+    val referer: String = "",    // 👈 أضف هذا السطر
+    val userAgent: String = "",  // 👈 وأضف هذا أيضاً للمستقبل
     val group: String = ""
 ) : Parcelable
-
 @Parcelize
 data class PackageItem(
     val name: String,
     val logo: String,
     val url: String
 ) : Parcelable
+
+fun Channel.toPlayerString(): String {
+    val realUrl = try {
+        String(Base64.decode(url, Base64.DEFAULT))
+    } catch (_: Exception) {
+        url
+    }
+
+    return if (drmLicense.isNotEmpty()) {
+        "$realUrl|drmLicense=$drmLicense"
+    } else {
+        realUrl
+    }
+}
